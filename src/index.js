262c4d01f3be07b5ea14bea460b1e56b60917eb9
Nick Jennings <nick@silverbucket.net>
2018-10-24 19:48:55 +0200

    dont track js

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_dom_1 = require("react-dom");
-require("./index.css");
-var registerServiceWorker_1 = require("./registerServiceWorker");
-react_dom_1.default.render((/>
-    < /HashRouter>), document.getElementById('root'));
-registerServiceWorker_1.default();
